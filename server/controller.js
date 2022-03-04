b21b02b58606fdca62c0aaa707d2fa29db94d6fd
Rockwell Allen <rockwellallen@gmail.com>
2022-03-03 19:12:08 -0800

    some route and controller structure

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,36 @@
 const axios = require('axios');
 
 module.exports = {
-  // type out your queries here.
+
+  getQuestions: function(req, res) => {
+
+  },
+
+  addQuestion: function(req, res) => {
+
+  },
+
+  getAnswers: function(req, res) => {
+
+  },
+
+  addAnswer: function(req, res) => {
+
+  },
+
+  helpfulQuestion: function(req, res) => {
+
+  },
+
+  reportQuestion: function(req, res) => {
+
+  },
+
+  helpfulAnswer: function(req, res) => {
+
+  },
+
+  reportAnswer: function(req, res) => {
+
+  }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,2 +1,31 @@
 var controller = require('./controller.js');
 const router = require ('express').Router();
+
+// get and add questions
+router.route('/qa/questions')
+  .get(controller.getQuestions)
+  .post(controller.addQuestion)
+
+// get and add answers
+router.route('/qa/questions/:question_id/answers')
+  .get(controller.getAnswers)
+  .post(controller.addAnswer)
+
+// mark question as helpful
+router.route('/qa/questions/:question_id/helpful')
+  .put(controller.helpfulQuestion)
+
+// report question
+router.route('/qa/questions/:question_id/report')
+  .put(controller.reportQuestion)
+
+// mark answer as helpful
+router.route('/qa/questions/:answer_id/helpful')
+  .put(controller.helpfulAnswer)
+
+// report an answer
+router.route('/qa/questions/:answer_id/report')
+  .put(controller.reportAnswer)
+
+
+module.exports router;
