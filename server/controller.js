52bc8e4542a8d11a1ad290296ab9dee86c1c20f7
Rockwell Allen <rockwellallen@gmail.com>
2022-03-05 13:11:36 -0800

    more formatting to get questions

--- server/controller.js
+++ server/controller.js
@@ -35,11 +35,13 @@ module.exports = {
           'question_helpfulness', question_helpfulness,
           'reported', questions.reported,
           'answers', (SELECT json_build_object(
-            'id', answer_id,
-            'body', body,
-            'date', answer_date,
-            'answerer_name', answerer_name,
-            'helpfulness', helpfulness
+            'answer_id', (SELECT json_build_object(
+              'id', answer_id,
+              'body', body,
+              'date', answer_date,
+              'answerer_name', answerer_name,
+              'helpfulness', helpfulness
+            ))
           ))
         ))) AS results FROM questions LEFT JOIN answers USING (question_id)
         WHERE product_id = 1 AND questions.reported = false GROUP BY product_id`;
