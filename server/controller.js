ca47b8ebf673e739da2dd787809b620e49aabb8b
Rockwell Allen <rockwellallen@gmail.com>
2022-03-06 20:42:42 -0800

    get answers completed

--- server/controller.js
+++ server/controller.js
@@ -23,46 +23,68 @@ module.exports = {
   //   'hepfulness', helpfulness
   // )) AS 68 FROM answers USING (question_id) GROUP BY question_id
 
-  getQuestions: (req, res) => {
-    console.log('working');
-    // need var for answers_phtotos.answer_id, product_id
-    const queryStr =
-      `SELECT product_id,
-        (SELECT array_agg (json_build_object(
-          'question_id', question_id,
-          'question_body', question_body,
-          'question_date', question_date,
-          'asker_name', asker_name,
-          'question_helpfulness', question_helpfulness,
-          'reported', questions.reported,
-          'answers', (SELECT json_build_object(
-            'answer_id', (SELECT json_build_object(
-              'id', answer_id,
-              'body', body,
-              'date', answer_date,
-              'answerer_name', answerer_name,
-              'helpfulness', helpfulness,
-              'photos', (select array_agg(json_build_object(
-                'id', id,
-                'url', photo_url
-              )) FROM answers_photos INNER JOIN answers
-              USING (answer_id) where answers_photos.answer_id = 5)
-            ))
-          ))
-        ))) AS results FROM questions
-          LEFT JOIN answers USING (question_id)
-        WHERE product_id = 1 AND questions.reported = false GROUP BY product_id`;
+      // `SELECT answer_id FROM answers INNER JOIN questions USING (question_id)`
+    // need resolve answers_photos.answer_id, product_id
+    // (SELECT CAST (answer_id AS INTEGER) FROM answers WHERE answers.question_id = questions.question_id)
 
+
+    // 'answers', (SELECT json_build_object(
+    //   '68', (SELECT json_build_object(
+    //     'id', answer_id,
+    //     'body', body,
+    //     'date', answer_date,
+    //     'answerer_name', answerer_name,
+    //     'helpfulness', helpfulness
+    //   ))
+    // ))
+    // 'photos', (select array_agg(json_build_object(
+    //   'id', id,
+    //   'url', photo_url
+    // )) FROM answers_photos INNER JOIN answers
+    // USING (answer_id) where answers_photos.answer_id = 5 GROUP BY answer_id)
+//-------
+    // const queryStr =
+    //   `SELECT product_id,
+    //     (SELECT array_agg (json_build_object(
+    //       'question_id', question_id,
+    //       'question_body', question_body,
+    //       'question_date', question_date,
+    //       'asker_name', asker_name,
+    //       'question_helpfulness', question_helpfulness,
+    //       'reported', questions.reported
+
+    //     ))) AS results FROM questions
+
+    //     WHERE product_id = $1 AND questions.reported = false
+    //     GROUP BY product_id`;
+
+        // GROUP BY questions.product_id
+        // LEFT JOIN answers USING (question_id)
     // const queryStr = `SELECT question_id, product_id, question_body, question_date,
     //   asker_name, reported, question_helpfulness
     //   FROM questions WHERE product_id = 1 AND reported = 'f' LIMIT 5`;
 
-    pool.query(queryStr, (err, results) => {
+  getQuestions: (req, res) => {
+    console.log('q getall query ', req.query);
+
+    const queryStr = ``
+
+    const queryArgs= [req.query.product_id];
+
+
+    pool.query(queryStr, queryArgs, (err, results) => {
       if (err) {
         console.log(err);
         res.status(404).send(err);
       } else {
-        res.status(200).send(results);
+        console.log(results);
+
+        // const results = {
+        //   product_id: req.query.product_id,
+        //   results:
+        // }
+        // you can redefine something to send
+        res.status(200).send(results.rows[0]);
       }
     });
   },
@@ -83,19 +105,40 @@ module.exports = {
   // },
 
   getAnswers: (req, res) => {
-    // coalesce
-    // SELECT array_agg(json_build_object('id', id, 'url', photo_url) as photos
-    const queryStr = `SELECT answer_id, body, answer_date AS date, answerer_name, helpfulness, (
-      SELECT array_agg(json_build_object('id', id, 'url', photo_url)) as photos
-        FROM answers_photos INNER JOIN answers
-        USING (answer_id) where answers_photos.answer_id = 5)
-      FROM answers WHERE answers.answer_id = 5 AND reported = 'f' LIMIT 5`;
+    // const queryStr = `SELECT answer_id, body, answer_date AS date, answerer_name, helpfulness, (
+    //   SELECT array_agg(json_build_object('id', id, 'url', photo_url)) as photos
+    //     FROM answers_photos INNER JOIN answers
+    //     USING (answer_id) where answers_photos.answer_id = 5)
+    //   FROM answers WHERE answers.question_id = 5 AND reported = 'f' LIMIT 5`;
+    console.log('r getall query ', req.params);
+    const queryStr =
+      `SELECT array_agg(json_build_object(
+        'answer_id', answer_id,
+        'body', body,
+        'date', answer_date,
+        'answerer_name', answerer_name,
+        'helfulness', helpfulness,
+        'photos', (
+          SELECT coalesce(array_agg(json_build_object(
+            'id', id,
+            'url', photo_url
+          )), '{}') FROM answers_photos ap WHERE a.answer_id = ap.answer_id)
+      )) AS results FROM answers a WHERE question_id = $1 AND reported = false`;
 
-    pool.query(queryStr, (err, results) => {
+      const queryArgs = [req.params.question_id];
+
+    pool.query(queryStr, queryArgs, (err, results) => {
       if (err) {
+        console.log(err)
         res.status(404).send(err);
       } else {
-        res.status(200).send(results);
+        let response = {
+          question: req.params.question_id,
+          page: req.params.page || 1,
+          count: req.params.count || 5,
+          results: results.rows[0].results
+        }
+        res.status(200).send(response);
       }
     });
   },
